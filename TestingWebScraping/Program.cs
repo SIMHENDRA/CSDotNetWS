5c4c390164d24ebfbe75672f455e5c718e254631
SIMHENDRA <ssn320@nyu.edu>
2020-10-22 23:08:24 -0400

    Add project files.

---
+++ TestingWebScraping/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Shamer_4001
+{
+    class Class1
+    {
+        public static void Main(String[] args)
+        {
+            WebClient wc = new WebClient();
+            wc.QueryString.Add("type", "aviation");
+            wc.QueryString.Add("role", "fighter");
+            wc.QueryString.Add("country", "all");
+            string res = wc.DownloadString("http://thunderskill.com/en/stat/DEFYN/vehicles/r");
+            System.IO.File.WriteAllText(@"op.txt", res);
+        }
+    }
+}
